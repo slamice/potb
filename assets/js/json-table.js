825db8e61cd610be5d2a039a4997eff404020c70
slamice <slamice@gmail.com>
2016-02-20 17:08:27 -0500

    added news and games

---
+++ assets/js/json-table.js
@@ -0,0 +1,56 @@
+function programTable(table_name, save_name, add_url, table_add, table_remove){
+    var $TABLE = $('#' + table_name);
+    var $save_button = $('#' + save_name);
+    var $add_url = '/' + add_url;
+    var $table_add = $('.' + table_add)
+    var $table_remove = $('.' + table_remove)
+
+    $table_add.click(function() {
+      var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+      $TABLE.find('table').append($clone);
+    });
+
+    $table_remove.click(function() {
+      $(this).parents('tr').detach();
+    });
+
+    // A few jQuery helpers for exporting only
+    jQuery.fn.pop = [].pop;
+    jQuery.fn.shift = [].shift;
+
+    $save_button.click(function() {
+      var $rows = $TABLE.find('tr:not(:hidden)');
+      var headers = [];
+      var data = [];
+
+      // Get the headers (add special header logic here)
+      $($rows.shift()).find('th:not(:empty)').each(function() {
+        headers.push($(this).text().toLowerCase());
+      });
+
+      // Turn all existing rows into a loopable array
+      $rows.each(function() {
+        var $td = $(this).find('td');
+        var h = {};
+
+        // Use the headers from earlier to name our hash keys
+        headers.forEach(function(header, i) {
+          h[header] = $td.eq(i).text();
+        });
+
+        data.push(h);
+      });
+
+      $.ajax({
+          url : $add_url,
+          type: "POST",
+          data: JSON.stringify(data),
+          contentType: "application/json; charset=utf-8",
+          dataType   : "json",
+          success    : function(){
+              console.log("Pure jQuery Pure JS object");
+          }
+      });
+
+    });
+}
--- main.go
+++ main.go
@@ -26,6 +26,34 @@ type Team struct {
 	Score   int64
 }
 
+// Performers class
+type Performer struct {
+	ID      int64 `db:"performer_id"`
+	Created int64
+	English_name    string `json:"english_name" form:"english_name" binding:"required"`
+	Japanese_name   string `json:"japanese_name" form:"japanese_name" binding:"required"`
+}
+
+// Game class
+type Game struct {
+	ID      int64 `db:"game_id"`
+	Created int64
+	English_name    string
+	Japanese_name   string
+	English_description    string
+	Japanese_description   string
+}
+
+// News class
+type News struct {
+	ID      int64 `db:"news_id"`
+	Created int64
+	Name string
+	Description    string
+}
+
+
+
 // AddTeam creates a new team
 func AddTeam(name string, color string) Team {
 	team := Team{
@@ -42,20 +70,87 @@ func AddTeam(name string, color string) Team {
 
 // AddScore adds the score by 1
 func AddScore(name string) {
-	_, err := dbmap.Exec("update teams set score = score + 1 where name = ?", name)
+	_, err := dbmap.Exec("update team set score = score + 1 where name = ?", name)
 	checkErr(err, "Update failed")
+}
+
+func AddNews(news []News) {
+	for i := range news {	
+		newsItem := News{
+			Created: time.Now().UnixNano(),
+			Name:   news[i].Name,
+			Description:  news[i].Description,
+		}
+
+		err := dbmap.Insert(&newsItem)
+		checkErr(err, "Creating new news failed")
+	}
+}
+
+// AddGames
+func AddGames(games []Game) {
+	for i := range games {	
+		game := Game{
+			Created: time.Now().UnixNano(),
+			English_name:   games[i].English_name,
+			Japanese_name:  games[i].Japanese_name,
+			English_description:    games[i].English_description,
+			Japanese_description:   games[i].Japanese_description,
+		}
+
+		err := dbmap.Insert(&game)
+		checkErr(err, "Creating a new performer failed")
+	}
+}
+
+// AddPerformers 
+func AddPerformers(performers []Performer) {
+	for i := range performers {	
+		perf := Performer{
+			Created: time.Now().UnixNano(),
+			English_name:   performers[i].English_name,
+			Japanese_name:  performers[i].Japanese_name,
+		}
+
+		err := dbmap.Insert(&perf)
+		checkErr(err, "Creating a new performer failed")
+	}
 }
 
 // GetTeams fetches all teams
 func GetTeams() []Team {
 	var teams []Team
-	_, err := dbmap.Select(&teams, "select * from teams order by score")
+	_, err := dbmap.Select(&teams, "select * from team order by score")
 	checkErr(err, "Select failed")
 	return teams
 }
 
-// TeamGet gets all teh teams in the db
-func TeamGet(c *gin.Context) {
+// GetPerformers fetches all performers
+func GetPerformers() []Performer {
+	var performers []Performer
+	_, err := dbmap.Select(&performers, "select * from performer")
+	checkErr(err, "Select failed")
+	return performers
+}
+
+// GetGames fetches all games
+func GetGames() []Game {
+	var games []Game
+	_, err := dbmap.Select(&games, "select * from game")
+	checkErr(err, "Select failed")
+	return games
+}
+
+// GetNews fetches all news
+func GetNews() []News {
+	var news []News
+	_, err := dbmap.Select(&news, "select * from news")
+	checkErr(err, "Select failed")
+	return news
+}
+
+// TeamsGet gets all teh teams in the db
+func TeamsGet(c *gin.Context) {
 	content := gin.H{}
 	for k, v := range GetTeams() {
 		content[strconv.Itoa(k)] = gin.H{
@@ -67,9 +162,70 @@ func TeamGet(c *gin.Context) {
 	c.JSON(200, content)
 }
 
+// NewsGet 
+func NewsGet(c *gin.Context) {
+	content := gin.H{}
+	for k, v := range GetNews() {
+		content[strconv.Itoa(k)] = gin.H{
+			"name": v.Name,
+			"description": v.Description,
+		}
+	}
+	c.JSON(200, content)
+}
+
+// GamesGet 
+func GamesGet(c *gin.Context) {
+	content := gin.H{}
+	for k, v := range GetGames() {
+		content[strconv.Itoa(k)] = gin.H{
+			"english_name": v.English_name,
+			"japanese_name": v.Japanese_name,
+		}
+	}
+	c.JSON(200, content)
+}
+
+
+// PerformersGet 
+func PerformersGet(c *gin.Context) {
+	content := gin.H{}
+	for k, v := range GetPerformers() {
+		content[strconv.Itoa(k)] = gin.H{
+			"english_name": v.English_name,
+			"japanese_name": v.Japanese_name,
+		}
+	}
+	c.JSON(200, content)
+}
+
+
+// ClearPerformers clears the performers to 0
+func ClearPerformers() {
+	deleteAllData("performer")
+}
+
+// Clear Games
+func ClearGames() {
+	deleteAllData("game")
+}
+
+// Clear News
+func ClearNews() {
+	deleteAllData("news")
+}
+
+// Delete all data
+func deleteAllData(table string) {
+	_, err := dbmap.Exec("DELETE FROM " + table)
+	checkErr(err, "Clear table failed")
+}
+
+
+
 // ClearScores clears the scores to 0
 func ClearScores() {
-	_, err := dbmap.Exec("update teams set score = 0")
+	_, err := dbmap.Exec("update team set score = 0")
 	checkErr(err, "Update failed")
 }
 
@@ -84,7 +240,46 @@ func ScorePut(c *gin.Context) {
 	c.JSON(200, content)
 }
 
-// ScorePost clears the score
+// PerformersPost 
+func PerformersPost(c *gin.Context) {
+	ClearPerformers()
+	var performers []Performer
+	err := c.Bind(&performers)
+	if err != nil {
+        panic (err)
+    }
+	AddPerformers(performers)
+	content := gin.H{"result": "Success"}
+	c.JSON(200, content)
+}
+
+// GamesPost
+func GamesPost(c *gin.Context) {
+	ClearGames()
+	var games []Game
+	err := c.Bind(&games)
+	if err != nil {
+        panic (err)
+    }
+	AddGames(games)
+	content := gin.H{"result": "Success"}
+	c.JSON(200, content)
+}
+
+// GamesPost
+func NewsPost(c *gin.Context) {
+	ClearNews()
+	var news []News
+	err := c.Bind(&news)
+	if err != nil {
+        panic (err)
+    }
+	AddNews(news)
+	content := gin.H{"result": "Success"}
+	c.JSON(200, content)
+}
+
+// ScorePost adds to the score
 func ScorePost(c *gin.Context) {
 	ClearScores()
 	content := gin.H{"result": "Success"}
@@ -95,11 +290,14 @@ func initDb() *gorp.DbMap {
 	db, err := sql.Open("sqlite3", "db.sqlite3")
 	checkErr(err, "sql.Open failed")
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
-	dbmap.AddTableWithName(Team{}, "teams").SetKeys(true, "ID")
+	dbmap.AddTableWithName(Team{}, "team").SetKeys(true, "ID")
+	dbmap.AddTableWithName(Performer{}, "performer").SetKeys(true, "ID")
+	dbmap.AddTableWithName(Game{}, "game").SetKeys(true, "ID")
+	dbmap.AddTableWithName(News{}, "news").SetKeys(true, "ID")
 	err = dbmap.CreateTablesIfNotExists()
 	checkErr(err, "Create tables failed")
 
-	count, err := dbmap.SelectInt("select count(*) from teams")
+	count, err := dbmap.SelectInt("select count(*) from team")
 	checkErr(err, "Create tables failed")
 	if count == 0 {
 		team := Team{
@@ -136,14 +334,29 @@ func main() {
 	// Statics
 	app.Static("/assets", "./assets")
 
+
 	app.PUT("/addscore", ScorePut)
 	app.POST("/clearscores", ScorePost)
-	app.GET("/getteams", TeamGet)
+	app.GET("/getteams", TeamsGet)
+
+	app.POST("/addperformers", PerformersPost)
+	app.GET("/getperformers", PerformersGet)
+
+	app.POST("/addgames", GamesPost)
+	app.GET("/getgames", GamesGet)
+
+	app.POST("/addnews", NewsPost)
+	app.GET("/getnews", NewsGet)
 
 	app.LoadHTMLGlob("templates/*")
 	app.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Pirates of Tokyo Bay!!!",
+		})
+	})
+	app.GET("/programs", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "programs.tmpl", gin.H{
+			"title": "Programs!!!",
 		})
 	})
 
