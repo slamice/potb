26e39c961fb1d5b166bc9be6a20208f4276a3f2d
slamice <slamice@gmail.com>
2016-09-19 02:24:14 -0400

    fixed date

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ type Program struct {
 
 // UpdateProgramDate replaces date for the program
 func UpdateProgramDate(programDate string) Program {
-	_, err := dbmap.Exec("update program set programddate = " + programDate)
+	_, err := dbmap.Exec(fmt.Sprintf("update program set programddate = '%s'", programDate))
 	checkErr(err, "Update failed for adding a new program date")
 
 	return GetProgram()
